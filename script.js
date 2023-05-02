f30943fd438b76a765fa0fcd5fabcd767f27af04
homem do acre <zecarminelli@gmail.com>
2023-05-02 17:13:02 -0300

    add página "pefil" + fix js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
-const inputFile = document.querySelector("#picture__input");
-const pictureImage = document.querySelector(".picture__image");
-const pictureImageTxt = "Escolha uma imagem";
 var sun = document.getElementById("sun");
-pictureImage.innerHTML = pictureImageTxt;
 const body = document.querySelector('body');
 const p = document.querySelector('div.mural > p');
 
@@ -32,27 +28,3 @@ sun.onclick = function(){
     p.style.color = '#1a1a1a'
   }
 }
-
-inputFile.addEventListener("change", function (e) {
-  const inputTarget = e.target;
-  const file = inputTarget.files[0];
-
-  if (file) {
-    const reader = new FileReader();
-
-    reader.addEventListener("load", function (e) {
-      const readerTarget = e.target;
-
-      const img = document.createElement("img");
-      img.src = readerTarget.result;
-      img.classList.add("picture__img");
-
-      pictureImage.innerHTML = "";
-      pictureImage.appendChild(img);
-    });
-
-    reader.readAsDataURL(file);
-  } else {
-    pictureImage.innerHTML = pictureImageTxt;
-  }
-});
