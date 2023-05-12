646d4f844fe8a0c554e5980c7ba000f215a8a000
homem do acre <zecarminelli@gmail.com>
2023-05-12 15:12:01 -0300

    darkmode fix 2.0

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,6 +1,7 @@
 var sun = document.getElementById("sun");
 const body = document.querySelector("body");
 const p = document.querySelector("div.mural > p");
+const h1 = document.querySelector("div#texto > h1");
 
 sun.onclick = function () {
   sun.classList.toggle("night");
@@ -14,11 +15,22 @@ sun.onclick = function () {
       p.style.color = "#1a1a1a";
     }
   } else {
-    if (this.classList.toggle("darkToggle")) {
-      body.style.background = "#1a1a1a";
-      body.style.transition = "1s";
+    if (h1) {
+      if (this.classList.toggle("darkToggle")) {
+        body.style.background = "#1a1a1a";
+        h1.style.color = "#ffffff";
+        body.style.transition = "1s";
+      } else {
+        body.style.background = "#ffffff";
+        h1.style.color = "#1a1a1a";
+      }
     } else {
-      body.style.background = "#ffffff";
+      if (this.classList.toggle("darkToggle")) {
+        body.style.background = "#1a1a1a";
+        body.style.transition = "1s";
+      } else {
+        body.style.background = "#ffffff";
+      }
     }
   }
 };
