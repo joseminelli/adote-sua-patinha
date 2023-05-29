1b3af29542ee46661d036767b3f8a1d89297359e
homem do acre <zecarminelli@gmail.com>
2023-05-29 17:33:01 -0300

    passa url da imagem  pro perfil de pet

--- js/exibir.js
+++ js/exibir.js
@@ -3,16 +3,22 @@ const idadepet = document.getElementById('idade');
 const raca1 = document.getElementById('raca');
 const descricao1 = document.getElementById('desc');
 
-    // Obtenha os valores dos parâmetros
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Recupera os dados do localStorage
     var nomepet2 = localStorage.getItem("nome");
     var idadepet2 = localStorage.getItem("idade");
     var raca = localStorage.getItem("raca");
     var descricao = localStorage.getItem("descricao");
+    var caminhoImagem = localStorage.getItem("imagempet");
 
-
+    // Exibe eles na página
     nomepet.innerHTML = nomepet2;
     idadepet.innerHTML = idadepet2 + " Anos";
     raca1.innerHTML = raca;
     descricao1.innerHTML = descricao;
 
+    var imgElement = document.getElementById("imagem");
+    imgElement.src = caminhoImagem;
 
+});
--- js/script.js
+++ js/script.js
@@ -14,22 +14,32 @@ if (close) {
     })
 }
 
-document.getElementById("enviar").addEventListener("click", function(event) {
-    event.preventDefault(); // Impede o envio do formulário padrão
+document.addEventListener("DOMContentLoaded", function() {
+    var enviarButton = document.getElementById("enviar");
+    if (enviarButton) {
+        enviarButton.addEventListener("click", function(event) {
+            event.preventDefault(); // Impede o envio do formulário padrão
 
-    // Obtenha os valores dos campos do formulário
-    var nome = document.getElementById("input").value;
-    var idade = document.getElementById("idade").value;
-    var raca = document.getElementById("raca").value;
-    var descricao = document.getElementById("descricao").value;
+            // Obtenha os valores dos campos do formulário
+            var nome = document.getElementById("input").value;
+            var idade = document.getElementById("idade").value;
+            var raca = document.getElementById("raca").value;
+            var descricao = document.getElementById("descricao").value;
 
-     // Armazene os dados no localStorage
-     localStorage.setItem("nome", nome);
-     localStorage.setItem("idade", idade);
-     localStorage.setItem("raca", raca);
-     localStorage.setItem("descricao", descricao);
+            // Armazene os dados no localStorage
+            localStorage.setItem("nome", nome);
+            localStorage.setItem("idade", idade);
+            localStorage.setItem("raca", raca);
+            localStorage.setItem("descricao", descricao);
 
-    // Redirecione para o outro HTML com os parâmetros na URL
-    //var url = "perfilp.html" + "?nome=" + encodeURIComponent(nome) + "&idade=" + encodeURIComponent(idade) + "&raca=" + encodeURIComponent(raca) + "&descricao=" + encodeURIComponent(descricao);
-    window.location.href = "perfilp.html";
+            // Obtenha o caminho da imagem selecionada
+            var inputImagem = document.getElementById("picture__input");
+            var caminhoImagem = inputImagem.value;
+
+            localStorage.setItem("imagem", caminhoImagem);
+
+            // Redirecione para a outra página
+            window.location.href = "perfilp.html";
+        });
+    }
 });
