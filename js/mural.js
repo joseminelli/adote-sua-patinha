0526e7b7df25c150c59a998f1086a2324aecff09
homem do acre <zecarminelli@gmail.com>
2023-06-06 13:52:14 -0300

    Filtro funcional

--- js/mural.js
+++ js/mural.js
@@ -3,7 +3,6 @@ import data from "./pets.json" assert { type: "json" };
 const fotopet = document.getElementById("imgpetc");
 var login = localStorage.getItem("login");
 const taNoMural = document.getElementById("formm");
-const taNoPerfilfake = document.getElementsByClassName("pefilFake");
 const mural = document.getElementById("mural");
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -13,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
   if (login != "true") {
     window.location.href = "index.html";
   }
+
   if (taNoMural) {
     var caminhoImagem2 = localStorage.getItem("imagempet");
 
@@ -22,13 +22,52 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
       imgElement2.src = caminhoImagem2;
     }
-    for (var i = 1; i < 13; i++) {
+
+    const regiaoSelect = document.getElementById("bairro2");
+    const idadeSelect = document.getElementById("idade");
+    const racaSelect = document.getElementById("raca");
+
+    regiaoSelect.addEventListener("change", atualizarFiltro);
+    idadeSelect.addEventListener("change", atualizarFiltro);
+    racaSelect.addEventListener("change", atualizarFiltro);
+
+    function atualizarFiltro() {
+      const regiaoSelecionada = regiaoSelect.value;
+      const idadeSelecionada = idadeSelect.value;
+      const racaSelecionada = racaSelect.value;
+
+      for (var i = 1; i <= 14; i++) {
+
+        const picfotopeti = document.getElementById(i.toString());
+        var pet = data.pets.find(FindByID);
+
+        const atendeFiltro =
+          (regiaoSelecionada === "0" || pet.regiao === regiaoSelecionada) &&
+          (idadeSelecionada === "" || pet.age.toString() === idadeSelecionada) &&
+          (racaSelecionada === "0" || pet.raca.toString() === racaSelecionada);
+
+        if (atendeFiltro) {
+          imgElement2.style.display = "inline-block";
+          picfotopeti.style.display = "inline-block";
+        } else {
+          imgElement2.style.display = "none";
+          picfotopeti.style.display = "none";
+        }
+
+        function FindByID(pet) {
+          return pet.id === i;
+        }
+      }
+    }
+
+    for (var i = 1; i <= 14; i++) { 
       loginId = i;
       var a = document.createElement("a");
 
       var picfotopeti = document.createElement("img");
-      picfotopeti.setAttribute("id",i.toString());
-      a.setAttribute("href", "perfilpf.html" + "?pet=" + i)
+      picfotopeti.setAttribute("id", i.toString());
+      picfotopeti.style.display = "inline-block";
+      a.setAttribute("href", "perfilpf.html" + "?pet=" + i);
       var pet = data.pets.find(FindByID);
       console.log(pet.name);
       picfotopeti.src = pet.image;
@@ -36,8 +75,8 @@ document.addEventListener("DOMContentLoaded", function () {
       function FindByID(pet) {
         return pet.id === i;
       }
-      a.appendChild(picfotopeti)
-      mural.appendChild(a)
+      a.appendChild(picfotopeti);
+      mural.appendChild(a);
     }
   }
 });
