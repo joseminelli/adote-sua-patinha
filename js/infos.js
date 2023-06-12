7054e1cea689504d06238ba213eb91d49590a9fc
José Minelli <zecarmineli@gmail.com>
2023-06-12 19:00:24 -0300

    json aplicado efetivamente

--- js/infos.js
+++ js/infos.js
@@ -1,37 +1,34 @@
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("js/pets.json")
-    .then((response) => response.json())
-    .then((data) => {
-      const url = new URL(window.location.href);
-      const numpet = url.searchParams.get("pet");
+document.addEventListener("DOMContentLoaded", async function () {
+  var response = await fetch("js/pets.json");
+  var data = await response.json();
+  const url = new URL(window.location.href);
+  const numpet = url.searchParams.get("pet");
 
-      const petInfo = data.pets.find(findByID);
-      console.log(petInfo);
+  const petInfo = data.pets.find(findByID);
+  console.log(petInfo);
 
-      function findByID(pet) {
-        return pet.id == numpet;
-      }
+  function findByID(pet) {
+    return pet.id == numpet;
+  }
 
-      if (petInfo) {
-        // Atualize os elementos HTML com as informações obtidas
+  if (petInfo) {
+    // Atualize os elementos HTML com as informações obtidas
 
-        const nomeElement = document.getElementById("name");
-        nomeElement.innerHTML = petInfo.name;
+    const nomeElement = document.getElementById("name");
+    nomeElement.innerHTML = petInfo.name;
 
-        const idadeElement = document.getElementById("idade");
-        idadeElement.innerHTML = petInfo.age;
+    const idadeElement = document.getElementById("idade");
+    idadeElement.innerHTML = petInfo.age;
 
-        const bairroElement = document.getElementById("raca");
-        bairroElement.innerHTML = petInfo.raca;
+    const bairroElement = document.getElementById("raca");
+    bairroElement.innerHTML = petInfo.raca;
 
-        const celElement = document.getElementById("desc");
-        celElement.innerHTML = petInfo.description;
+    const celElement = document.getElementById("desc");
+    celElement.innerHTML = petInfo.description;
 
-        const picElement = document.getElementById("imagem");
-        picElement.src = petInfo.image;
-      } else {
-        console.log("Pet não encontrado");
-      }
-      
-    });
+    const picElement = document.getElementById("imagem");
+    picElement.src = petInfo.image;
+  } else {
+    console.log("Pet não encontrado");
+  }
 });
