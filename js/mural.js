0edcae89119a917b9a31d733dde66aadeb1fcb9b
homem do acre <zecarminelli@gmail.com>
2023-06-12 14:31:42 -0300

    favoritos funcional porém feio

--- js/mural.js
+++ js/mural.js
@@ -7,7 +7,6 @@ const mural = document.getElementById("mural");
 
 document.addEventListener("DOMContentLoaded", function () {
   let loginId;
-  data.pets.push();
 
   const chkFavoritos = document.getElementById("chkFavoritos");
   const favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
@@ -15,117 +14,119 @@ document.addEventListener("DOMContentLoaded", function () {
   if (login != "true") {
     window.location.href = "index.html";
   }
-  fetch("js/pets.json")
-    .then((response) => response.json())
-    .then((responseJson) => {
-      if (taNoMural) {
-        var caminhoImagem2 = localStorage.getItem("imagempet");
+  if (taNoMural) {
+    var caminhoImagem2 = localStorage.getItem("imagempet");
 
-        var imgElement2 = document.getElementById("imgpetc");
-        if (caminhoImagem2 == null) {
-          imgElement2.style.display = "none";
-        } else {
-          imgElement2.src = caminhoImagem2;
-        }
+    var imgElement2 = document.getElementById("imgpetc");
+    if (caminhoImagem2 == null) {
+      imgElement2.style.display = "none";
+    } else {
+      imgElement2.src = caminhoImagem2;
+    }
 
-        const regiaoSelect = document.getElementById("bairro2");
-        const idadeSelect = document.getElementById("idade");
-        const racaSelect = document.getElementById("raca");
-        const especieSelect = document.getElementById("especie");
+    const regiaoSelect = document.getElementById("bairro2");
+    const idadeSelect = document.getElementById("idade");
+    const racaSelect = document.getElementById("raca");
+    const especieSelect = document.getElementById("especie");
 
-        regiaoSelect.addEventListener("change", atualizarFiltro);
-        idadeSelect.addEventListener("change", atualizarFiltro);
-        racaSelect.addEventListener("change", atualizarFiltro);
-        especieSelect.addEventListener("change", atualizarFiltro);
-        chkFavoritos.addEventListener("change", atualizarFiltro);
+    regiaoSelect.addEventListener("change", atualizarFiltro);
+    idadeSelect.addEventListener("change", atualizarFiltro);
+    racaSelect.addEventListener("change", atualizarFiltro);
+    especieSelect.addEventListener("change", atualizarFiltro);
+    chkFavoritos.addEventListener("change", atualizarFiltro);
 
-        function atualizarFiltro() {
-          const apenasFavoritos = chkFavoritos.checked;
-          const regiaoSelecionada = regiaoSelect.value;
-          const idadeSelecionada = idadeSelect.value;
-          const racaSelecionada = racaSelect.value;
-          const especieSelecionada = especieSelect.value;
+    function atualizarFiltro() {
+      const apenasFavoritos = chkFavoritos.checked;
+      const regiaoSelecionada = regiaoSelect.value;
+      const idadeSelecionada = idadeSelect.value;
+      const racaSelecionada = racaSelect.value;
+      const especieSelecionada = especieSelect.value;
 
-          for (var i = 1; i <= 14; i++) {
-            const petId = i.toString();
-            const picfotopeti = document.getElementById(petId);
-            var pet = data.pets.find(FindByID);
-            const isFavorito = favoritos.includes(petId);
+      for (var i = 2; i <= 13; i++) {
+        var pet = data.pets.find(FindByID);
+        const petId = i.toString();
+        var picfotopeti = document.getElementById(petId);
+        var btnfavorito = document.getElementsByClassName("botaofav" + petId);
+        
+        const isFavorito = favoritos.includes(petId);
+       
+        const atendeFiltro =
+          (regiaoSelecionada === "0" || pet.regiao === regiaoSelecionada) &&
+          (idadeSelecionada === "" ||
+            pet.age.toString() === idadeSelecionada) &&
+          (especieSelecionada === "0" ||
+            pet.esp.toString() === especieSelecionada) &&
+          (racaSelecionada === "0" || pet.raca.toString() === racaSelecionada);
 
-            const atendeFiltro =
-              (regiaoSelecionada === "0" || pet.regiao === regiaoSelecionada) &&
-              (idadeSelecionada === "" ||
-                pet.age.toString() === idadeSelecionada) &&
-              (especieSelecionada === "0" ||
-                pet.esp.toString() === especieSelecionada) &&
-              (racaSelecionada === "0" ||
-                pet.raca.toString() === racaSelecionada);
-
-            if (atendeFiltro && (isFavorito || !apenasFavoritos)) {
-              picfotopeti.style.display = "inline-block";
-              if (caminhoImagem2 != null) {
-                imgElement2.style.display = "inline-block";
-              }
-            } else {
-              imgElement2.style.display = "none";
-              picfotopeti.style.display = "none";
-            }
-
-            function FindByID(pet) {
-              return pet.id === i;
-            }
-          }
-        }
-
-        for (var i = 2; i <= 14; i++) {
-          loginId = i;
-          const petId = i.toString();
-          var a = document.createElement("a");
-          var a2 = document.createElement("a");
-
-          const favoritoBtn = document.createElement("button");
-          favoritoBtn.classList.add("btn-favorito");
-          favoritoBtn.dataset.petId = petId - 1;
-
-          a2.setAttribute("href", "perfilpf.html" + "?pet=" + i);
-          var pet = data.pets.find(FindByID);
-          console.log(pet.name);
-
-          var picfotopeti = document.createElement("img");
-          picfotopeti.setAttribute("id", i.toString());
+        if (atendeFiltro && (isFavorito || !apenasFavoritos)) {
           picfotopeti.style.display = "inline-block";
-          picfotopeti.src = pet.image;
-
-          if (favoritos.includes(petId)) {
-            favoritoBtn.classList.add("favoritado");
+          console.log(btnfavorito)
+          //btnfavorito.style.display = "inline-block";
+          if (caminhoImagem2 != null) {
+            imgElement2.style.display = "inline-block";
           }
+        } else {
+          imgElement2.style.display = "none";
+          picfotopeti.style.display = "none";
+          //btnfavorito.style.display = "none";
+        }
 
-          favoritoBtn.addEventListener("click", function () {
-            const petId = this.dataset.petId;
-
-            if (favoritos.includes(petId)) {
-              favoritos.splice(favoritos.indexOf(petId), 1);
-              this.classList.remove("favoritado");
-            } else {
-              favoritos.push(petId);
-              this.classList.add("favoritado");
-            }
-
-            localStorage.setItem("favoritos", JSON.stringify(favoritos));
-
-            var fav = localStorage.getItem("favoritos");
-            console.log(fav);
-          });
-
-          function FindByID(pet) {
-            return pet.id === i;
-          }
-
-          a.appendChild(favoritoBtn);
-          a2.appendChild(picfotopeti);
-          mural.appendChild(a);
-          mural.appendChild(a2);
+        function FindByID(pet) {
+          return pet.id === i;
         }
       }
-    });
+    }
+
+    for (var i = 2; i <= 13; i++) {
+      loginId = i;
+      const petId = i.toString();
+      var a = document.createElement("a");
+      var a2 = document.createElement("a");
+
+      const picfotopeti = document.createElement("img");
+      picfotopeti.setAttribute("id", i.toString());
+      picfotopeti.style.display = "inline-block";
+
+      const favoritoBtn = document.createElement("button");
+      favoritoBtn.classList.add("btn-favorito");
+      favoritoBtn.classList.add("botaofav" + petId);
+      favoritoBtn.dataset.petId = petId - 1;
+
+      a2.setAttribute("href", "perfilpf.html" + "?pet=" + i);
+      var pet = data.pets.find(FindByID);
+      console.log(pet.name);
+
+      picfotopeti.src = pet.image;
+
+      if (favoritos.includes(petId)) {
+        favoritoBtn.classList.add("favoritado");
+      }
+
+      favoritoBtn.addEventListener("click", function () {
+        const petId = this.dataset.petId;
+
+        if (favoritos.includes(petId)) {
+          favoritos.splice(favoritos.indexOf(petId), 1);
+          this.classList.remove("favoritado");
+        } else {
+          favoritos.push(petId);
+          this.classList.add("favoritado");
+        }
+
+        localStorage.setItem("favoritos", JSON.stringify(favoritos));
+
+        var fav = localStorage.getItem("favoritos");
+        console.log(fav);
+      });
+
+      a.appendChild(favoritoBtn);
+      a2.appendChild(picfotopeti);
+      mural.appendChild(a);
+      mural.appendChild(a2);
+
+      function FindByID(pet) {
+        return pet.id === i;
+      }
+    }
+  }
 });
