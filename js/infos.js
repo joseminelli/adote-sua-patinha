29b8d2d51f5858a65dba85b3e90a904f224a6d0c
José Minelli <zecarmineli@gmail.com>
2023-06-12 18:35:43 -0300

    fix total do ios

--- js/infos.js
+++ js/infos.js
@@ -1,35 +1,36 @@
-import data from "./pets.json" assert { type: "json" };
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("js/pets.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const url = new URL(window.location.href);
+      const numpet = url.searchParams.get("pet");
 
-document.addEventListener('DOMContentLoaded', function() {
-    const url = new URL(window.location.href);
-    const numpet = url.searchParams.get('pet');
-  
-    const petInfo = data.pets.find(findByID);
-    console.log(petInfo)
-  
-    function findByID(pet){
-      return pet.id == numpet;
-    }
-  
-    if (petInfo) {
-      // Atualize os elementos HTML com as informações obtidas
-  
-      const nomeElement = document.getElementById('name');
-      nomeElement.innerHTML = petInfo.name;
-  
-      const idadeElement = document.getElementById('idade');
-      idadeElement.innerHTML = petInfo.age;
-  
-      const bairroElement = document.getElementById('raca');
-      bairroElement.innerHTML = petInfo.raca;
-  
-      const celElement = document.getElementById('desc');
-      celElement.innerHTML = petInfo.description;
-  
-      const picElement = document.getElementById('imagem');
-      picElement.src = petInfo.image;
-    } else {
-      console.log('Pet não encontrado');
-    }
-  });
-  
+      const petInfo = data.pets.find(findByID);
+      console.log(petInfo);
+
+      function findByID(pet) {
+        return pet.id == numpet;
+      }
+
+      if (petInfo) {
+        // Atualize os elementos HTML com as informações obtidas
+
+        const nomeElement = document.getElementById("name");
+        nomeElement.innerHTML = petInfo.name;
+
+        const idadeElement = document.getElementById("idade");
+        idadeElement.innerHTML = petInfo.age;
+
+        const bairroElement = document.getElementById("raca");
+        bairroElement.innerHTML = petInfo.raca;
+
+        const celElement = document.getElementById("desc");
+        celElement.innerHTML = petInfo.description;
+
+        const picElement = document.getElementById("imagem");
+        picElement.src = petInfo.image;
+      } else {
+        console.log("Pet não encontrado");
+      }
+    });
+});
