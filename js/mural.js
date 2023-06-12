339dd0a1e944f525e65a0eba50721a3e95ed20fc
José Minelli <zecarmineli@gmail.com>
2023-06-12 11:37:53 -0300

    primeira versão dos favoritos

--- js/mural.js
+++ js/mural.js
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", function () {
   let loginId;
   data.pets.push();
 
+  const favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+
   if (login != "true") {
     window.location.href = "index.html";
   }
@@ -34,26 +36,31 @@ document.addEventListener("DOMContentLoaded", function () {
         idadeSelect.addEventListener("change", atualizarFiltro);
         racaSelect.addEventListener("change", atualizarFiltro);
         especieSelect.addEventListener("change", atualizarFiltro);
-        
-          function atualizarFiltro() {
-         
-    
+
+        function atualizarFiltro() {
+          const apenasFavoritos =
+            document.getElementById("apenas-favoritos").checked;
           const regiaoSelecionada = regiaoSelect.value;
           const idadeSelecionada = idadeSelect.value;
           const racaSelecionada = racaSelect.value;
           const especieSelecionada = especieSelect.value;
 
           for (var i = 1; i <= 14; i++) {
-            const picfotopeti = document.getElementById(i.toString());
+            const petId = i.toString();
+            const picfotopeti = document.getElementById(petId);
             var pet = data.pets.find(FindByID);
+            const isFavorito = favoritos.includes(petId);
 
             const atendeFiltro =
               (regiaoSelecionada === "0" || pet.regiao === regiaoSelecionada) &&
-              (idadeSelecionada === "" || pet.age.toString() === idadeSelecionada) &&
-              (especieSelecionada === "0" || pet.esp.toString() === especieSelecionada) &&
-              (racaSelecionada === "0" || pet.raca.toString() === racaSelecionada);
+              (idadeSelecionada === "" ||
+                pet.age.toString() === idadeSelecionada) &&
+              (especieSelecionada === "0" ||
+                pet.esp.toString() === especieSelecionada) &&
+              (racaSelecionada === "0" ||
+                pet.raca.toString() === racaSelecionada);
 
-            if (atendeFiltro) {
+            if (atendeFiltro && (isFavorito || !apenasFavoritos)) {
               picfotopeti.style.display = "inline-block";
               if (caminhoImagem2 != null) {
                 imgElement2.style.display = "inline-block";
@@ -71,19 +78,47 @@ document.addEventListener("DOMContentLoaded", function () {
 
         for (var i = 1; i <= 14; i++) {
           loginId = i;
+          const petId = i.toString();
           var a = document.createElement("a");
+
+          const favoritoBtn = document.createElement("button");
+          favoritoBtn.classList.add("btn-favorito");
+          favoritoBtn.dataset.petId = petId;
 
           var picfotopeti = document.createElement("img");
           picfotopeti.setAttribute("id", i.toString());
           picfotopeti.style.display = "inline-block";
-          a.setAttribute("href", "perfilpf.html" + "?pet=" + i);
+          //a.setAttribute("href", "perfilpf.html" + "?pet=" + i);
           var pet = data.pets.find(FindByID);
           console.log(pet.name);
           picfotopeti.src = pet.image;
 
+          if (favoritos.includes(petId)) {
+            favoritoBtn.classList.add("favoritado");
+          }
+
+          favoritoBtn.addEventListener("click", function () {
+            const petId = this.dataset.petId;
+
+            if (favoritos.includes(petId)) {
+              favoritos.splice(favoritos.indexOf(petId), 1);
+              this.classList.remove("favoritado");
+            } else {
+              favoritos.push(petId);
+              this.classList.add("favoritado");
+            }
+
+            localStorage.setItem("favoritos", JSON.stringify(favoritos));
+
+            var fav = localStorage.getItem("favoritos");
+            console.log(fav);
+          });
+
           function FindByID(pet) {
             return pet.id === i;
           }
+
+          a.appendChild(favoritoBtn);
           a.appendChild(picfotopeti);
           mural.appendChild(a);
         }
