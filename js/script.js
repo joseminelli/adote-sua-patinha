b4b48d17714bc5303f5c0f0130d967db6eb71fde
homem do acre <zecarminelli@gmail.com>
2023-06-15 18:31:14 -0300

    Teste de corte de imagem

--- js/script.js
+++ js/script.js
@@ -1,182 +1,219 @@
-const bar = document.getElementById('bar');
-const close = document.getElementById('close');
-const nav = document.getElementById('navbar');
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
 var logado = false;
 
 function CustomAlert() {
-    this.alert = function (message, title) {
-      var overlay = document.createElement('div');
-      overlay.id = 'overlay';
-      overlay.className = 'animado';
-  
-      var dialogBox = document.createElement('div');
-      dialogBox.id = 'dialogbox';
-      dialogBox.className = 'slit-in-vertical';
-  
-      var dialogContent = document.createElement('div');
-      dialogContent.innerHTML = '<div id="alertHeader"></div><div id="alertBody"></div><div id="alertFooter"></div>';
-  
-      dialogBox.appendChild(dialogContent);
-  
-      var body = document.getElementsByTagName('body')[0];
-      body.appendChild(overlay);
-      body.appendChild(dialogBox);
-  
-      overlay.style.height = window.innerHeight + 'px';
-      overlay.style.display = 'block';
-      dialogBox.style.display = 'block';
-  
-      var alertHeader = document.getElementById('alertHeader');
-      if (typeof title === 'undefined') {
-        alertHeader.style.display = 'none';
-      } else {
-        alertHeader.innerHTML = '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> ' + title;
-      }
-  
-      document.getElementById('alertBody').innerHTML = message;
-      document.getElementById('alertFooter').innerHTML =
-        '<button class="pure-material-button-contained active" onclick="customAlert.ok()">OK</button>';
-    };
-  
-    this.ok = function () {
-      var overlay = document.getElementById('overlay');
-      var dialogBox = document.getElementById('dialogbox');
-      overlay.parentNode.removeChild(overlay);
-      dialogBox.parentNode.removeChild(dialogBox);
-    };
-  }
-  
-  var customAlert = new CustomAlert();
-  
-  
-  
+  this.alert = function (message, title) {
+    var overlay = document.createElement("div");
+    overlay.id = "overlay";
+    overlay.className = "animado";
+
+    var dialogBox = document.createElement("div");
+    dialogBox.id = "dialogbox";
+    dialogBox.className = "slit-in-vertical";
+
+    var dialogContent = document.createElement("div");
+    dialogContent.innerHTML =
+      '<div id="alertHeader"></div><div id="alertBody"></div><div id="alertFooter"></div>';
+
+    dialogBox.appendChild(dialogContent);
+
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(overlay);
+    body.appendChild(dialogBox);
+
+    overlay.style.height = window.innerHeight + "px";
+    overlay.style.display = "block";
+    dialogBox.style.display = "block";
+
+    var alertHeader = document.getElementById("alertHeader");
+    if (typeof title === "undefined") {
+      alertHeader.style.display = "none";
+    } else {
+      alertHeader.innerHTML =
+        '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> ' + title;
+    }
+
+    document.getElementById("alertBody").innerHTML = message;
+    document.getElementById("alertFooter").innerHTML =
+      '<button class="pure-material-button-contained active" onclick="customAlert.ok()">OK</button>';
+  };
+
+  this.ok = function () {
+    var overlay = document.getElementById("overlay");
+    var dialogBox = document.getElementById("dialogbox");
+    overlay.parentNode.removeChild(overlay);
+    dialogBox.parentNode.removeChild(dialogBox);
+  };
+}
+
+var customAlert = new CustomAlert();
 
 if (bar) {
-    bar.addEventListener('click', () => {
-        nav.classList.add('active');
-    })
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
 }
 
 if (close) {
-    close.addEventListener('click', () => {
-        nav.classList.remove('active');
-    })
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    var enviarButton = document.getElementById("enviar"); // Botão do cadastro de pet
-    var enviarButton2 = document.getElementById("enviar2"); // Botão do cadastro de pessoas
-    var loginButton = document.getElementById("login"); // Botão do cadastro de pessoas
-    var login = localStorage.getItem("login");
-    if (enviarButton) {
-        //não acessa a página se não tiver login
-        if (login != "true") {
-            window.location.href = "index.html";
-        }
-        enviarButton.addEventListener("click", function (event) {
-            event.preventDefault();
-
-            var nome = document.getElementById("input").value;
-            var idade = document.getElementById("idade").value;
-            var raca = document.getElementById("raca").value;
-            var descricao = document.getElementById("descricao").value;
-
-            localStorage.setItem("nome", nome);
-            localStorage.setItem("idade", idade);
-            localStorage.setItem("raca", raca);
-            localStorage.setItem("descricao", descricao);
-
-            if (nome === "" || descricao === "" || raca === "0") {
-                new customAlert.alert('Você precisa preencher todos os campos','Atenção!');
-                return;
-            }
-
-            var inputImagem = document.getElementById("picture__input");
-
-            if (inputImagem.files && inputImagem.files[0]) {
-                var imagem = inputImagem.files[0];
-
-                var reader = new FileReader();
-
-                reader.onload = function (e) {
-                    var imagemBase64 = e.target.result;
-
-                    localStorage.setItem("imagempet", imagemBase64);
-
-
-                };
-
-                reader.readAsDataURL(imagem);
-            } else { //obriga a colocar uma imagem
-                customAlert.alert('Seu pet precisa de uma foto','Atenção!');
-                return
-            }
-
-            logado = true;
-            localStorage.setItem('login', logado);
-            window.location.href = "perfilp.html";
-        });
+  var enviarButton = document.getElementById("enviar"); // Botão do cadastro de pet
+  var enviarButton2 = document.getElementById("enviar2"); // Botão do cadastro de pessoas
+  var loginButton = document.getElementById("login"); // Botão do cadastro de pessoas
+  var login = localStorage.getItem("login");
+  if (enviarButton) {
+    //não acessa a página se não tiver login
+    if (login != "true") {
+      window.location.href = "index.html";
     }
-    if (enviarButton2) {
-        //cria as opcões de idade dinamicamente
-        var selectIdade = document.getElementById("idade2");
+    enviarButton.addEventListener("click", function (event) {
+      event.preventDefault();
 
-        for (var i = 18; i <= 100; i++) {
-            var option = document.createElement("option");
-            option.setAttribute("value", i);
-            option.textContent = i;
-            selectIdade.appendChild(option);
-        }
+      var nome = document.getElementById("input").value;
+      var idade = document.getElementById("idade").value;
+      var raca = document.getElementById("raca").value;
+      var descricao = document.getElementById("descricao").value;
 
-        enviarButton2.addEventListener("click", function (event) {
-            event.preventDefault();
+      localStorage.setItem("nome", nome);
+      localStorage.setItem("idade", idade);
+      localStorage.setItem("raca", raca);
+      localStorage.setItem("descricao", descricao);
 
-            var nome2 = document.getElementById("input2").value;
-            var idade2 = document.getElementById("idade2").value;
-            var bairro = document.getElementById("bairro2").value;
-            var telefone = document.getElementById("telefone").value;
-            if (nome2 === "" || telefone === "" || bairro === "0") {
-                customAlert.alert('Você precisa preencher todos os campos','Atenção!');
-                return
-            }
+      if (nome === "" || descricao === "" || raca === "0") {
+        new customAlert.alert(
+          "Você precisa preencher todos os campos",
+          "Atenção!"
+        );
+        return;
+      }
 
-            localStorage.setItem("nome2", nome2);
-            localStorage.setItem("idade2", idade2);
-            localStorage.setItem("bairro", bairro);
-            localStorage.setItem("telefone", telefone);
+      var inputImagem = document.getElementById("picture__input");
 
-            var inputImagem2 = document.getElementById("picture__input");
-            const inputImagem3 = document.getElementsByClassName("picture");
+      if (inputImagem.files && inputImagem.files[0]) {
+        var imagem = inputImagem.files[0];
 
-            if (inputImagem2.files && inputImagem2.files[0]) {
-                var imagem2 = inputImagem2.files[0];
+        var reader = new FileReader();
 
-                var reader = new FileReader();
+        reader.onload = function (e) {
+          var imagemBase64 = e.target.result;
 
-                reader.onload = function (e) {
-                    var imagemBase642 = e.target.result;
-                    localStorage.setItem("imagempessoa", imagemBase642);
-                };
-                reader.readAsDataURL(imagem2);
-            } else {
-                customAlert.alert('Você precisa escolher uma foto','Atenção!');
-                return
-            }
-            logado = true;
-            localStorage.setItem("login", logado);
-            window.location.href = "perfil.html";
-        });
+          localStorage.setItem("imagempet", imagemBase64);
+        };
+
+        reader.readAsDataURL(imagem);
+      } else {
+        //obriga a colocar uma imagem
+        customAlert.alert("Seu pet precisa de uma foto", "Atenção!");
+        return;
+      }
+
+      logado = true;
+      localStorage.setItem("login", logado);
+      window.location.href = "perfilp.html";
+    });
+  }
+  if (enviarButton2) {
+    //cria as opcões de idade dinamicamente
+    var selectIdade = document.getElementById("idade2");
+
+    for (var i = 18; i <= 100; i++) {
+      var option = document.createElement("option");
+      option.setAttribute("value", i);
+      option.textContent = i;
+      selectIdade.appendChild(option);
     }
-    if (loginButton) {
-        loginButton.addEventListener("click", function (event) {
-            login2 = localStorage.getItem("login");
-            if (login2 === "true") {
-                window.location.href = "perfil.html";
-            } else {
-                customAlert.alert('Você precisa criar uma conta primeiro','Atenção!');
-                return
-            }
-        });
-    }
+
+    enviarButton2.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      var nome2 = document.getElementById("input2").value;
+      var idade2 = document.getElementById("idade2").value;
+      var bairro = document.getElementById("bairro2").value;
+      var telefone = document.getElementById("telefone").value;
+      if (nome2 === "" || telefone === "" || bairro === "0") {
+        customAlert.alert("Você precisa preencher todos os campos", "Atenção!");
+        return;
+      }
+
+      localStorage.setItem("nome2", nome2);
+      localStorage.setItem("idade2", idade2);
+      localStorage.setItem("bairro", bairro);
+      localStorage.setItem("telefone", telefone);
+
+      var inputImagem2 = document.getElementById("picture__input");
+      const inputImagem3 = document.getElementsByClassName("picture");
+
+      if (inputImagem2.files && inputImagem2.files[0]) {
+        var imagem2 = inputImagem2.files[0];
+
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+          var imagemBase642 = e.target.result;
+
+          var img = new Image();
+          img.src = imagemBase642;
+          img.onload = function () {
+            var canvas = document.createElement("canvas");
+            var ctx = canvas.getContext("2d");
+
+            // Defina o tamanho desejado do recorte
+            var cropWidth = 550;
+            var cropHeight = 550;
+
+            // Defina as coordenadas iniciais do recorte
+            var cropX = (img.width - cropWidth) / 2;
+            var cropY = (img.height - cropHeight) / 2;
+
+            // Ajuste o tamanho do canvas para o tamanho do recorte
+            canvas.width = cropWidth;
+            canvas.height = cropHeight;
+
+            // Desenhe a imagem recortada no canvas
+            ctx.drawImage(
+              img,
+              cropX,
+              cropY,
+              cropWidth,
+              cropHeight,
+              0,
+              0,
+              cropWidth,
+              cropHeight
+            );
+
+            // Obtenha a imagem recortada como base64
+            var croppedImageBase64 = canvas.toDataURL();
+
+            localStorage.setItem("imagempessoa", croppedImageBase64);
+            console.log(croppedImageBase64)
+          };
+        };
+        reader.readAsDataURL(imagem2);
+      } else {
+        customAlert.alert("Você precisa escolher uma foto", "Atenção!");
+        return;
+      }
+      logado = true;
+      localStorage.setItem("login", logado);
+      window.location.href = "perfil.html";
+    });
+  }
+  if (loginButton) {
+    loginButton.addEventListener("click", function (event) {
+      login2 = localStorage.getItem("login");
+      if (login2 === "true") {
+        window.location.href = "perfil.html";
+      } else {
+        customAlert.alert("Você precisa criar uma conta primeiro", "Atenção!");
+        return;
+      }
+    });
+  }
 });
-
