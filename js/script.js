179b20236f9fa58eee19c59fe2f1229513be8556
homem do acre <zecarminelli@gmail.com>
2023-07-11 20:42:12 -0300

    fix

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const bar = document.getElementById("bar");
 const close = document.getElementById("close");
 const nav = document.getElementById("navbar");
 const body2 = document.querySelector("body");
+const express = require('express');
 var logado = false;
 
 const section = document.getElementById("modalNovo"),
