6842bcb4dd745b4ac684a045554b2f6982e49307
homem do acre <zecarminelli@gmail.com>
2023-07-14 16:43:43 -0300

    teste cadstro de pessoa

--- backend/server.js
+++ backend/server.js
@@ -58,6 +58,60 @@ app.post("/salvar", (req, res) => {
   });
 });
 
+app.post("/salvarPessoa", (req, res) => {
+  if (fs.existsSync("../../usuarios.json") === false) {
+    fs.writeFile(
+      "../../usuarios.json",
+      '{"pets": []}',
+      () => {}
+    );
+  }
+  console.log(req.body);
+  const nome = req.body.nome;
+  const idade = req.body.idade;
+  const bairro = req.body.bairro;
+  const telefone = req.body.telefone;
+  const email = req.body.email;
+  const senha = req.body.senha;
+  const pets = req.body.senha;
+  const imagem = req.body.imagem; 
+
+  fs.readFile("../../usuarios.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Erro ao ler o arquivo JSON");
+      return;
+    }
+
+    let jsonData = JSON.parse(data);
+    const newId = jsonData.pets.length + 1;
+
+    const newPessoa = {
+      id: newId,
+      name: nome,
+      age: idade,
+      bairro: bairro,
+      telefone: telefone,
+      email: email,
+      senha: senha,
+      pets: pets,
+      image: imagem,
+    };
+
+    jsonData.pets.push(newPessoa);
+    console.log(JSON.stringify(newPessoa));
+    fs.writeFile("../../usuarios.json", JSON.stringify(jsonData), (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Erro ao salvar os dados");
+      } else {
+        res.send("Dados salvos com sucesso");
+      }
+    });
+  });
+});
+
+
 app.listen(port, () => {
   console.log(`Servidor ouvindo na porta ${port}`);
 });
